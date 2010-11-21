16cb7751466a963c56ae50d26a924d4d181dcf2f
scott <devnull@localhost>
2010-11-21 16:27:32 -0600

    added initial proj

---
+++ KilnLockdown/KilnLocker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FogCreek.FogBugz;
+using FogCreek.FogBugz.Plugins;
+using FogCreek.FogBugz.Plugins.Api;
+using FogCreek.FogBugz.Plugins.Interfaces;
+using FogCreek.FogBugz.UI;
+using FogCreek.FogBugz.UI.Dialog;
+using FogCreek.FogBugz.Plugins.Entity;
+
+namespace KilnLockdown
+{
+    public class KilnLocker : Plugin, IPluginPersonDisplay
+    {
+        public KilnLocker(CPluginApi api) : base(api) { }
+
+        public CDialogItem[] PersonDisplayEdit(CPerson person)
+        {
+            CDialogItem[] retVal = null;
+            var editDisplayHTML = new StringBuilder();
+
+            //if (IsEligible(person))
+            {
+                editDisplayHTML.Append("<input type='radio' name='useKiln' id='useKilnTrue' value='true' />");
+                editDisplayHTML.Append("<label for='useKilnTrue'>Yes</label>");
+                editDisplayHTML.Append("<input type='radio' name='useKiln' id='useKilnFalse' value='false' />");
+                editDisplayHTML.Append("<label for='useKilnFalse'>No</label>");
+
+                retVal = new CDialogItem[] { new CDialogItem(editDisplayHTML.ToString(), "Allow Kilin", "Is this user allowed to use kiln?") };
+            }
+
+            return retVal;
+        }
+
+        private bool IsEligible(CPerson person)
+        {
+            return api.SiteConfiguration.IsKilnEnabled && !person.IsSiteAdmin();
+        }
+
+        public string[] PersonDisplayListFields(CPerson person)
+        {
+            return new string[] { "PersonDisplayListFields 1 " + person.sFullName };
+        }
+
+        public string[] PersonDisplayListHeaders()
+        {
+            return new string[] { "PersonDisplayListHeaders 2" };
+        }
+    }
+}
---
+++ KilnLockdown/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using FogCreek.Plugins;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kiln Lock Down")]
+[assembly: AssemblyDescription("Prevents access to Kiln for those who don't need it.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Scoarescoare Software")]
+[assembly: AssemblyProduct("KilnLockdown")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d57c26ac-592f-4018-9795-acb7610187af")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+/*FogCreek Plugin Info*/
+[assembly: AssemblyFogCreekPluginIdAttribute("scoarescoare_KLD")]
+[assembly: AssemblyFogCreekMajorVersionAttribute(3)]
+[assembly: AssemblyFogCreekMinorVersionMinAttribute(5)]
+[assembly: AssemblyFogCreekEmailAddressAttribute("scoarescoare@scoarescoare.com")]
+[assembly: AssemblyFogCreekWebsiteAttribute("http://scoarescoare.com")]
