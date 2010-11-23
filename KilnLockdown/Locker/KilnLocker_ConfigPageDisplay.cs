95b325b7bc293fcd2419be6d54917528836b8c8f
scott <devnull@localhost>
2010-11-23 09:32:40 -0600

    added config page

---
+++ KilnLockdown/Locker/KilnLocker_ConfigPageDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FogCreek.FogBugz;
+using FogCreek.FogBugz.Plugins;
+using FogCreek.FogBugz.Plugins.Api;
+using FogCreek.FogBugz.Plugins.Interfaces;
+using FogCreek.FogBugz.UI;
+using FogCreek.FogBugz.UI.Dialog;
+using FogCreek.FogBugz.Plugins.Entity;
+
+namespace KilnLockdown.Locker
+{
+    public partial class KilnLocker : IPluginConfigPageDisplay
+    {
+        public string ConfigPageDisplay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
